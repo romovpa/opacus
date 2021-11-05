8703562ecaa1970f02e270c6c83a970a08f5132d
Peter Romov <romovpa@fb.com>
2021-11-05 16:22:06 +0000

    First draft of Lightning API for Opacus

--- examples/mnist_lightning.py
+++ examples/mnist_lightning.py
@@ -16,15 +16,15 @@ $ tensorboard --logdir=lightning_logs/
 """
 
 import warnings
-from typing import Optional
+from typing import Optional, Any
 
 import pytorch_lightning as pl
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchmetrics
-from opacus import PrivacyEngine
-from opacus.utils.uniform_sampler import UniformWithReplacementSampler
+from opacus.lightning import OpacusCallback
 from pytorch_lightning.utilities.cli import LightningCLI
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
@@ -37,34 +37,16 @@ class LitSampleConvNetClassifier(pl.LightningModule):
     def __init__(
         self,
         lr: float = 0.1,
-        enable_dp: bool = True,
-        delta: float = 1e-5,
-        sample_rate: float = 0.001,
-        sigma: float = 1.0,
-        max_per_sample_grad_norm: float = 1.0,
-        secure_rng: bool = False,
     ):
         """A simple conv-net for classifying MNIST with differential privacy
 
         Args:
             lr: Learning rate
-            enable_dp: Enables training with privacy guarantees using Opacus (if True), vanilla SGD otherwise
-            delta: Target delta for which (eps, delta)-DP is computed
-            sample_rate: Sample rate used for batch construction
-            sigma: Noise multiplier
-            max_per_sample_grad_norm: Clip per-sample gradients to this norm
-            secure_rng: Use secure random number generator
         """
         super().__init__()
 
         # Hyper-parameters
         self.lr = lr
-        self.enable_dp = enable_dp
-        self.delta = delta
-        self.sample_rate = sample_rate
-        self.sigma = sigma
-        self.max_per_sample_grad_norm = max_per_sample_grad_norm
-        self.secure_rng = secure_rng
 
         # Parameters
         self.conv1 = nn.Conv2d(1, 16, 8, 2, padding=3)
@@ -109,41 +91,13 @@ class LitSampleConvNetClassifier(pl.LightningModule):
         self.log("test_accuracy", self.test_accuracy, on_step=False, on_epoch=True)
         return loss
 
-    # Adding differential privacy learning
-
-    def on_train_start(self) -> None:
-        if self.enable_dp:
-            self.privacy_engine = PrivacyEngine(
-                self,
-                sample_rate=self.sample_rate,
-                alphas=[1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64)),
-                noise_multiplier=self.sigma,
-                max_grad_norm=self.max_per_sample_grad_norm,
-                secure_rng=self.secure_rng,
-            )
-
-            optimizer = self.optimizers()
-            self.privacy_engine.attach(optimizer)
-
-    def on_train_epoch_end(self):
-        if self.enable_dp:
-            epsilon, best_alpha = self.privacy_engine.get_privacy_spent(self.delta)
-            # Privacy spent: (epsilon, delta) for alpha
-            self.log("epsilon", epsilon, on_epoch=True, prog_bar=True)
-            self.log("alpha", best_alpha, on_epoch=True, prog_bar=True)
-
-    def on_train_end(self):
-        if self.enable_dp:
-            self.privacy_engine.detach()
 
 
 class MNISTDataModule(pl.LightningDataModule):
     def __init__(
         self,
+        test_batch_size: int = 1000,
         data_dir: Optional[str] = "../mnist",
-        test_batch_size: int = 1024,
-        sample_rate: float = 0.001,
-        secure_rng: bool = False,
     ):
         """MNIST DataModule with DP-ready batch sampling
 
@@ -158,19 +112,6 @@ class MNISTDataModule(pl.LightningDataModule):
         self.dataloader_kwargs = {"num_workers": 1, "pin_memory": True}
 
         self.save_hyperparameters()
-
-        if secure_rng:
-            try:
-                import torchcsprng as prng
-            except ImportError as e:
-                msg = (
-                    "To use secure RNG, you must install the torchcsprng package! "
-                    "Check out the instructions here: https://github.com/pytorch/csprng#installation"
-                )
-                raise ImportError(msg) from e
-            self.generator = prng.create_random_device_generator("/dev/urandom")
-        else:
-            self.generator = None
 
     @property
     def transform(self):
@@ -193,11 +134,6 @@ class MNISTDataModule(pl.LightningDataModule):
         )
         return DataLoader(
             train_dataset,
-            batch_sampler=UniformWithReplacementSampler(
-                num_samples=len(train_dataset),
-                sample_rate=self.hparams.sample_rate,
-                generator=self.generator,
-            ),
             **self.dataloader_kwargs,
         )
 
@@ -213,6 +149,25 @@ class MNISTDataModule(pl.LightningDataModule):
             shuffle=True,
             **self.dataloader_kwargs,
         )
+
+
+def main():
+    data = MNISTDataModule()
+    model = LitSampleConvNetClassifier()
+
+    privacy_callback = OpacusCallback()
+    privacy_data = privacy_callback.wrap_datamodule(data)
+
+    trainer = pl.Trainer(
+        max_epochs=2,
+        enable_model_summary=False,
+        callbacks=[privacy_callback],
+    )
+    trainer.fit(model, privacy_data)
+
+    trainer.test(model, data)
+    trainer.test(model, privacy_data)  # both work
+
 
 
 def cli_main():
@@ -231,4 +186,5 @@ def cli_main():
 
 
 if __name__ == "__main__":
-    cli_main()
+    #cli_main()
+    main()
